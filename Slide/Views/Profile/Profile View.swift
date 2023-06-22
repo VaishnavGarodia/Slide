58c4883e14964fa6345691fa9a5ecd0b33647946
ethanharianto <ethanhanshr919@gmail.com>
2023-06-22 00:23:46 -0400

    fixed username bug

--- Slide/Views/Profile/Profile View.swift
+++ Slide/Views/Profile/Profile View.swift
@@ -8,36 +8,22 @@
 import Firebase
 import SwiftUI
 
-struct Profile_View: View {
-    @State public var recSize = 100.0
-    @State public var numEvents = 1
-    @State public var numHighlights = 1
-    @State public var numSaved = 1
-    @State public var user = Auth.auth().currentUser
-//
-//    func downloadpic()  -> Image {
-//        var profilePic = Image("")
-//        var profilePicRef = storageRef.child("profilePics/" + (user?.uid ?? "default") + ".jpg")
-//        profilePicRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
-//
-//          if let error = error {
-//            return
-//          } else {
-//              profilePic =  Image(uiImage: UIImage(data: data) ?? "ProfilePic")
-//          }
-//        }
-//        return profilePic
-//    }
+struct ProfileView: View {
+    @State private var recSize = 100.0
+    @State private var numEvents = 1
+    @State private var numHighlights = 1
+    @State private var numSaved = 1
+    @State private var user = Auth.auth().currentUser
     
     var body: some View {
         VStack {
             ZStack {
                 Circle()
                     .frame(width: 120.0, height: 120.0)
-                    .foregroundStyle(.linearGradient(Gradient(colors: [.accentColor, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .foregroundColor(Color(.blue))
                     .padding()
                 
-                ProfilePictureView(profilePictureURL: user?.photoURL ?? URL(string: "https://t3.ftcdn.net/jpg/01/65/63/94/360_F_165639425_kRh61s497pV7IOPAjwjme1btB8ICkV0L.jpg"))
+                ProfilePictureView()
             }
             
             Text(user?.displayName ?? "")
@@ -55,14 +41,6 @@ struct Profile_View: View {
                 ScrollView(.horizontal) {
                     HStack {
                         ForEach(0..<numEvents, id: \.self) { _ in
-//                            ZStack {
-//                                RoundedRectangle(cornerRadius: 25)
-//                                    .frame(width: recSize, height: recSize, alignment: .leading)
-//                                .foregroundColor(.gray)
-//
-//                                Image(systemName: "plus")
-//                            }
-                            
                             ZStack {
                                 RoundedRectangle(cornerRadius: 25)
                                     .frame(width: recSize, height: recSize, alignment: .leading)
@@ -77,7 +55,7 @@ struct Profile_View: View {
                 HStack {
                     Text("Highlights")
                         .font(.title2)
-                        
+                    
                     Image(systemName: "light.ribbon")
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
@@ -85,26 +63,21 @@ struct Profile_View: View {
                 
                 ScrollView(.horizontal) {
                     HStack {
-                        ZStack {
-                            Image("Event1")
-                                .resizable()
-                                .clipShape(RoundedRectangle(cornerRadius: 25))
-                                .frame(width: recSize, height: recSize)
-                        }
+                        Image("Event1")
+                            .resizable()
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                            .frame(width: recSize, height: recSize)
                         
-                        ZStack {
-                            Image("Event2")
-                                .resizable()
-                                .clipShape(RoundedRectangle(cornerRadius: 25))
-                                .frame(width: recSize, height: recSize)
-                        }
+                        Image("Event2")
+                            .resizable()
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                            .frame(width: recSize, height: recSize)
                         
-                        ZStack {
-                            Image("Event3")
-                                .resizable()
-                                .clipShape(RoundedRectangle(cornerRadius: 25))
-                                .frame(width: recSize, height: recSize)
-                        }
+                        Image("Event3")
+                            .resizable()
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                            .frame(width: recSize, height: recSize)
+                        
                         ForEach(0..<numHighlights, id: \.self) { _ in
                             ZStack {
                                 RoundedRectangle(cornerRadius: 25)
@@ -128,36 +101,31 @@ struct Profile_View: View {
                 
                 ScrollView(.horizontal) {
                     HStack {
-                        ZStack {
-                            Image("Saved1")
-                                .resizable()
-                                .clipShape(RoundedRectangle(cornerRadius: 25))
-                                .frame(width: recSize, height: recSize)
-                        }
-                        ZStack {
-                            Image("Saved2")
-                                .resizable()
-                                .clipShape(RoundedRectangle(cornerRadius: 25))
-                                .frame(width: recSize, height: recSize)
-                        }
-                        ZStack {
-                            Image("Saved3")
-                                .resizable()
-                                .clipShape(RoundedRectangle(cornerRadius: 25))
-                                .frame(width: recSize, height: recSize)
-                        }
-                        ZStack {
-                            Image("Saved4")
-                                .resizable()
-                                .clipShape(RoundedRectangle(cornerRadius: 25))
-                                .frame(width: recSize, height: recSize)
-                        }
-                        ZStack {
-                            Image("Saved5")
-                                .resizable()
-                                .clipShape(RoundedRectangle(cornerRadius: 25))
-                                .frame(width: recSize, height: recSize)
-                        }
+                        Image("Saved1")
+                            .resizable()
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                            .frame(width: recSize, height: recSize)
+                        
+                        Image("Saved2")
+                            .resizable()
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                            .frame(width: recSize, height: recSize)
+                        
+                        Image("Saved3")
+                            .resizable()
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                            .frame(width: recSize, height: recSize)
+                        
+                        Image("Saved4")
+                            .resizable()
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                            .frame(width: recSize, height: recSize)
+                        
+                        Image("Saved5")
+                            .resizable()
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                            .frame(width: recSize, height: recSize)
+                        
                         ForEach(0..<numSaved, id: \.self) { _ in
                             ZStack {
                                 RoundedRectangle(cornerRadius: 25)
@@ -174,8 +142,8 @@ struct Profile_View: View {
     }
 }
 
-struct Profile_View_Previews: PreviewProvider {
+struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        Profile_View()
+        ProfileView()
     }
 }
