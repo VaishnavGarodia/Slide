e503da619ca305046ed7fd5d7ed3124c6f4169d7
tshundi <thomasshundi@gmail.com>
2023-08-12 22:17:19 -0400

    Integrated event preview into creation process

--- Slide/Views/Map/ShowEvents/EventDetailsView.swift
+++ Slide/Views/Map/ShowEvents/EventDetailsView.swift
@@ -5,30 +5,50 @@
 import SwiftUI
 
 struct EventDetailsView: View {
-    var event: EventData
-
+    var image: UIImage = UIImage()
+    var bannerURL: String
+    var icon: String
+    var name: String
+    var description: String
+    var host: String
+    var start: String
+    var end: String
+    
+    
     var body: some View {
         VStack {
+            
+
+            
             // Display event details here based on the 'event' parameter
             // For example:
-            if !event.bannerURL.isEmpty {
-                EventBanner(imageURL: URL(string: event.bannerURL)!)
+            if image != UIImage() {
+                // TODO: Ethan please fix display here. This is what will display when we use preview since we haven't technically uploaded to firebase yet.
+                Text("This One")
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 50) // Adjust the width and height as needed
+
+            }
+            else if !bannerURL.isEmpty {
+                EventBanner(imageURL: URL(string: bannerURL)!)
                     .cornerRadius(15)
             }
             HStack {
-                Image(systemName: event.icon)
+                Image(systemName: icon)
                     .imageScale(.large)
-                Text(event.name)
+                Text(name)
                     .font(.title)
             }
             
-            Text(event.eventDescription)
+            Text(description)
                 .font(.body)
             // ... (display other details as needed)
-            Text(event.host)
+            Text(host)
             HStack {
-                Text(event.start)
-                Text(event.end)
+                Text(start)
+                Text(end)
             }
             Spacer()
         }
