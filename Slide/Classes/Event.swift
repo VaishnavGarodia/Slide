e60d83c48e9ac97680edd1401cfec4ede6bfcafc
Vaishnav Garodia <vaishnav@hippocraticai.com>
2023-08-24 14:33:13 +0530

    Working hype system

--- Slide/Classes/Event.swift
+++ Slide/Classes/Event.swift
@@ -11,7 +11,8 @@ class Event: NSObject, MKAnnotation {
     var coordinate: CLLocationCoordinate2D
     var slides, highlights: [String]
     
-    init(name: String, description: String, host: String, address: String, start: Date, end: Date, hostUID: String, icon: String, coordinate: CLLocationCoordinate2D, bannerURL: String, hype: String, id: String, slides: [String], highlights: [String]) {
+    init(name: String, description: String, host: String, address: String, start: Date, end: Date, hostUID: String, icon: String, coordinate: CLLocationCoordinate2D, bannerURL: String, hype: String, id: String, slides: [String], highlights: [String], hypestEventScore: Int) {
+
         self.name = name
         self.title = name
         self.eventDescription = description
@@ -28,6 +29,33 @@ class Event: NSObject, MKAnnotation {
         self.id = id
         self.slides = slides
         self.highlights = highlights
+        let hypeAmount: Int = slides.count + 2*highlights.count
+        if (hypeAmount>hypestEventScore){
+            // Reference to Firestore database
+            let db = Firestore.firestore()
+
+            // Reference to the document
+            let docRef = db.collection("HypestEventScore").document("hypestEventScore")
+
+            // Update the document with the new score
+            docRef.updateData(["score": hypeAmount]) { error in
+                if let error = error {
+                    print("Error updating document: \(error)")
+                } else {
+                    print("Document successfully updated with new score.")
+                }
+            }
+        }
+        if (self.hype==""){
+            let hypeScore: Float = Float((hypeAmount/hypestEventScore) * 100)
+            if (hypeScore>0 && hypeScore<30){
+                self.hype = "low"
+            } else if (hypeScore>30 && hypeScore<70) {
+                self.hype = "medium"
+            } else if (hypeScore>70){
+                self.hype = "high"
+            }
+        }
     }
 
     override init() {
