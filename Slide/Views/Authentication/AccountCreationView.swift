27a8f2d134bb1d6d879c74013792e67646538e29
tshundi <thomasshundi@gmail.com>
2023-09-09 15:30:37 -0400

    Walkthrough added and event gallery filtering

--- Slide/Views/Authentication/AccountCreationView.swift
+++ Slide/Views/Authentication/AccountCreationView.swift
@@ -7,6 +7,8 @@ import SwiftUI
 struct AccountCreationView: View {
     /* the logIn variable here is passed as the binding variable into the LogIn and Register views. This is done so that the AccountCreationBottom struct used in both views are able to toggle the logIn variable despite being separated by two layers. This then allows us to use a transition rather than the animation-less navigation link. */
     @State private var logIn = true
+    @Binding var userSignedUp: Bool
+
 
     var body: some View {
         VStack {
@@ -14,17 +16,17 @@ struct AccountCreationView: View {
                 LogIn(logIn: $logIn)
                     // comes in from the left
                     .transition(.move(edge: .leading))
+                    .onAppear {
+                        userSignedUp = false
+                    }
             } else {
                 Register(logIn: $logIn)
                     // comes in from the right
                     .transition(.move(edge: .trailing))
+                    .onAppear {
+                        userSignedUp = true
+                    }
             }
         }
-    }
-}
-
-struct AccountCreationView_Previews: PreviewProvider {
-    static var previews: some View {
-        AccountCreationView()
     }
 }
